44b62c43783c9f52c137071483a4061d91e61fd4
Holden Mitchell <holdenmitchell@users.noreply.github.com>
2023-07-28 15:14:58 -0400

    Initial commit

---
+++ lib/ok2explore.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ok2explore/version"
+
+module Ok2explore
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ok2explore/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Ok2explore
+  VERSION = "0.1.0"
+end
