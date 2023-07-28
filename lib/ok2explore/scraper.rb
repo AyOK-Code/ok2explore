4777f3522623e346ea65a159d678d17541d45939
Holden Mitchell <holdenmitchell@users.noreply.github.com>
2023-07-28 15:34:38 -0400

    Add scraper that takes the params of the form

--- lib/ok2explore/scraper.rb
+++ lib/ok2explore/scraper.rb
@@ -2,57 +2,50 @@ require 'selenium-webdriver'
 
 module Ok2explore
   class Scraper
-    # Configure Chrome driver options
-options = Selenium::WebDriver::Chrome::Options.new
-options.add_argument('--headless')  # Run Chrome in headless mode
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
+    attr_accessor :first_name, :last_name, :month, :year
 
-# Set Chrome driver path
-#driver_path = '/usr/local/bin/chromedriver'  # Replace with th# Configure Chrome driver options
+    def initialize(first_name, last_name, month, year)
+      @first_name = first_name
+      @last_name = last_name
+      @month = month
+      @year = year
+    end
 
-# Set Chrome driver service
-#Selenium::WebDriver::Chrome.driver_path = driver_path
+    def perform
+      # Configure Chrome driver options
+      options = Selenium::WebDriver::Chrome::Options.new
+      options.add_argument('--headless')  # Run Chrome in headless mode
+      options.add_argument('--no-sandbox')
+      options.add_argument('--disable-dev-shm-usage')
+      driver = Selenium::WebDriver.for(:chrome, options: options)
+      driver.get('https://ok2explore.health.ok.gov/App/DeathSearch')
 
-# Initialize Selenium WebDriver
-driver = Selenium::WebDriver.for(:chrome, options: options)
+      # Find and fill in the first name field
+      first_name_input = driver.find_element(:id, 'FirstName')
+      first_name_input.send_keys(first_name)
 
-# Navigate to the webpage
-driver.get('https://ok2explore.health.ok.gov/App/DeathSearch')
+      # Find and fill in the last name field
+      last_name_input = driver.find_element(:id, 'LastName')
+      last_name_input.send_keys(last_name)
 
-# Find and fill in the first name field
-first_name_input = driver.find_element(:id, 'FirstName')
-first_name_input.send_keys('J*')
+      # Find and select the month
+      month_option = driver.find_element(:id, 'DeathMonth')
+      month_option.send_keys(month)
 
-# Find and fill in the last name field
-last_name_input = driver.find_element(:id, 'LastName')
-last_name_input.send_keys('J*')
+      # Find and select the year
+      year_option = driver.find_element(:id, 'DeathYear')
+      year_option.send_keys(year)  # Assuming '1998' represents the desired year value
+      submit_button = driver.find_element(:id, 'deathSearchButton')
+      submit_button.click
 
-# Find and select the month
-month_option = driver.find_element(:id, 'DeathMonth')
-month_option.send_keys('2')  # Assuming '4' represents the desired month value
-
-# Find and select the year
-year_option = driver.find_element(:id, 'DeathYear')
-year_option.send_keys('1998')  # Assuming '1998' represents the desired year value
-
-# Submit the form
-submit_button = driver.find_element(:id, 'deathSearchButton')
-submit_button.click
-
-# Wait for the page to load and perform further actions as needed
-# Wait for the search results to load
-wait = Selenium::WebDriver::Wait.new(timeout: 10)  # Adjust the timeout value as needed
-results_div = wait.until { driver.find_element(:id, 'gvDeathResults') }
-
-# Access the browser session
-session_storage = driver.execute_script('return window.sessionStorage')
-# Access specific values from session storage
-value = session_storage['deathResults']
-
-puts value
-# Close the browser
-driver.quit
+      # Wait for the page to load and perform further actions as needed
+      wait = Selenium::WebDriver::Wait.new(timeout: 10)  # Adjust the timeout value as needed
+      results_div = wait.until { driver.find_element(:id, 'gvDeathResults') }
+      session_storage = driver.execute_script('return window.sessionStorage')
+      
+      driver.quit
+      return session_storage['deathResults']
+    end
   end
 end
 
